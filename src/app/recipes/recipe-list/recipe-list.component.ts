ceee9d0851385858cd8397ea13fa26c2639a4886
Zhaonian Luan <zhaonial@uci.edu>
2017-12-22 20:40:08 -0800

    added clickable item with details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,7 @@ import { Recipe } from '../recipe.model';
         styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+        @Output() recipeWasSelected = new EventEmitter<Recipe>();
         recipes: Recipe[] = [
                 new Recipe(
                         'A Test Recipe',
@@ -15,8 +15,8 @@ export class RecipeListComponent implements OnInit {
                         'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'
                 ),
                 new Recipe(
-                        'A Test Recipe',
-                        'This is simply a test',
+                        'Another Test Recipe',
+                        'This is simply another test',
                         'http://s3.amazonaws.com/finecooking.s3.tauntonclud.com/app/uploads/2017/04/18180350/051SIP112-grilled-mustard-rosemary-chicken-recipe-alt-main.jpg'
                 )
         ];
@@ -26,4 +26,7 @@ export class RecipeListComponent implements OnInit {
         ngOnInit() {
         }
 
+        onRecipeSelected(recipe: Recipe) {
+                this.recipeWasSelected.emit(recipe);
+        }
 }
