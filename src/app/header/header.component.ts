22f22e77330ca51c486f6f0aeba30eab85209097
Zhaonian Luan <zhaonial@uci.edu>
2017-12-22 17:22:46 -0800

    split navigation of recipes and shopping-list

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
         selector: 'app-header',
@@ -6,4 +6,9 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
 
+        @Output() featureSelcted = new EventEmitter<string>();
+
+        onSelect(feature: string) {
+                this.featureSelcted.emit(feature);
+        }
 }
