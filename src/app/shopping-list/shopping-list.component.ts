51a198b7912f923912d1629ca5914faf9e6ac87a
Zhaonian Luan <zhaonial@uci.edu>
2017-12-25 13:21:18 -0800

    changed EventEmitter to rxjs.Subject

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
         selector: 'app-shopping-list',
         templateUrl: './shopping-list.component.html',
         styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
         ingredients: Ingredient[];
+        private subscription: Subscription;
 
         constructor(private slService: ShoppingListService) { }
 
         ngOnInit() {
                 this.ingredients = this.slService.getIngredients();
-                this.slService.ingredientsChanged.subscribe(
+                this.subscription = this.slService.ingredientsChanged.subscribe(
                         (ingredients: Ingredient[]) => {
                                 this.ingredients = ingredients;
                         }
                 );
+        }
+
+        ngOnDestroy() {
+                this.subscription.unsubscribe();
         }
 }
