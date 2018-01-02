146442fa40c1e0786007ec9a5ae4284bedc787af
Zhaonian Luan <zhaonial@uci.edu>
2018-01-02 01:06:06 -0800

    cleaned up core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -14,14 +13,12 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
         declarations: [
                 AppComponent,
-                HeaderComponent,
-                HomeComponent,
         ],
         imports: [
                 HttpModule,
@@ -29,7 +26,8 @@ import { HomeComponent } from './home/home.component';
                 AppRoutingModule,
                 SharedModule,
                 ShoppingListModule,
-                AuthModule
+                AuthModule,
+                CoreModule
         ],
         providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
         bootstrap: [AppComponent]
