72a9bbcc5b312728bd33740e30f1de2009f45000
Zhaonian Luan <zhaonial@uci.edu>
2018-01-02 16:07:13 -0800

    added type any to HttpRequest

--- src/app/shared/logging.interceptor.ts
+++ src/app/shared/logging.interceptor.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/do';
 
 export class LoggingInterceptor implements HttpInterceptor {
-        intercept(req: HttpRequest, next: HttpHandler): Observable<HttpEvent<any>> {
+        intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
                 return next.handle(req).do(
                         event => {
                                 console.log('Logging interceptor' + event);
